0199cfb12b2781edeccabe388f6f0eb251218987
Christian n <christianhndr@gmail.com>
2024-01-31 17:53:16 +0700

    five commit

--- js/script.js
+++ js/script.js
@@ -6,11 +6,43 @@ function validateForm() {
     if (namee=="" || emaill=="" || interestt=="") {
         alert("tidak boleh kosong");
         return false;
+    } else {
+        alert("data telah kami terima mohon menunggu 1x24 jam")
     }
 
-    
+    setSenderUI(namee,emaill,interestt);
+    return false;
 
 }
 
+function setSenderUI(namee,emaill,interestt){
+    document.getElementById("info1").innerHTML = namee;
+    document.getElementById("info2").innerHTML = emaill;
+    document.getElementById("info3").innerHTML = interestt;
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+    var i;
+    var imgList = document.getElementsByClassName("slideshow");
+    if (n > imgList.length) slideIndex=1;
+    else if (n < 1) slideIndex = imgList.length;
+
+     for (i = 0; i < imgList.length; i++) {
+        imgList[i].style.display = "none";
+     }
+
+     imgList[slideIndex - 1].style.display = "block";
+}
+
+setInterval(() => {
+    plusDivs(1);
+}, 1100)
 
 
