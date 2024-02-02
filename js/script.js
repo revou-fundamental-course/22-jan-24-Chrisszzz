a824ae9ef691e40bf575b8953072856baef4b9f7
Christian n <christianhndr@gmail.com>
2024-02-02 19:54:33 +0700

    revisi konten

--- js/script.js
+++ js/script.js
@@ -11,13 +11,17 @@ function validateForm() {
     }
 }
 
+//IMAGE
+//deklarasi variabel slide index
 var slideIndex = 1;
 showDivs(slideIndex);
 
+//buat function untuk button slider (next, previous)
 function plusDivs(n) {
     showDivs((slideIndex += n));
 }
 
+//buat function slideshow
 function showDivs(n) {
     var i;
     var imgList = document.getElementsByClassName("slideshow");
@@ -31,8 +35,39 @@ function showDivs(n) {
      imgList[slideIndex - 1].style.display = "block";
 }
 
+//set interval pergantian gambar (1100=1,1s) 
 setInterval(() => {
     plusDivs(1);
-}, 1100)
+}, 2000)
+
+
+//FEED
+//deklarasi variabel untuk feed 
+var feedIndex = 1;
+showFeed(feedIndex);
+
+//buat function untuk button slider (next, previous)
+function plusFeed(n) {
+    showFeed((feedIndex += n));
+}
+
+//buat function slideshow di feed
+function showFeed(n) {
+    var i;
+    var feedList = document.getElementsByClassName("feed");
+    if (n > feedList.length) feedIndex=1;
+    else if (n < 1) feedIndex = feedList.length;
+
+     for (i = 0; i < feedList.length; i++) {
+        feedList[i].style.display = "none";
+     }
+
+     feedList[feedIndex - 1].style.display = "block";
+}
+
+//set interval pergantian feed (1100=1,1s) 
+setInterval(() => {
+    plusFeed(1);
+}, 2000)
 
 
