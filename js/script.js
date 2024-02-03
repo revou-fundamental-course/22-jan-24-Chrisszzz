b987bc19f7fdd548ac92f29701dd41fba2ba229b
Christian n <christianhndr@gmail.com>
2024-02-03 12:14:23 +0700

    menambahkan fitur dot

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ function validateForm() {
     const emaill = document.forms["msgform"]["email"].value;
     const interestt = document.forms["msgform"]["intr"].value;
 
-    if (namee=="" || emaill=="" || interestt=="") {
-        alert("tidak boleh kosong");
+    if (namee=="" || emaill=="" || interestt=="choose your option") {
+        alert("data tidak boleh kosong");
         return false;
     } else {
         alert("data telah kami terima mohon menunggu 1x24 jam")
     }
+
 }
 
 //IMAGE
@@ -21,18 +22,27 @@ function plusDivs(n) {
     showDivs((slideIndex += n));
 }
 
+// Thumbnail image controls / dot img
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
 //buat function slideshow
 function showDivs(n) {
     var i;
     var imgList = document.getElementsByClassName("slideshow");
+    let dots = document.getElementsByClassName("dot");
     if (n > imgList.length) slideIndex=1;
     else if (n < 1) slideIndex = imgList.length;
 
      for (i = 0; i < imgList.length; i++) {
         imgList[i].style.display = "none";
      }
-
+     for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
      imgList[slideIndex - 1].style.display = "block";
+     dots[slideIndex-1].className += " active";
 }
 
 //set interval pergantian gambar (1100=1,1s) 
@@ -65,9 +75,5 @@ function showFeed(n) {
      feedList[feedIndex - 1].style.display = "block";
 }
 
-//set interval pergantian feed (1100=1,1s) 
-setInterval(() => {
-    plusFeed(1);
-}, 2000)
 
 
